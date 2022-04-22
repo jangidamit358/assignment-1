9a99a28e2883a734b599741fa243ff2d6e716ad1
Amit surendra jangid <69582482+jangidamit358@users.noreply.github.com>
2022-04-22 11:47:16 +0530

    Update trending.js

--- src/components/trending.js
+++ src/components/trending.js
@@ -7,16 +7,17 @@ import { LoadingWrapper } from "./loading/LoadingWrapper";
 import { Dot } from "./loading/Dot";
 import { Pagination } from "./pagination";
 export default function Trending() {
-  const [data, setData] = useState([]);
-  const [page, setpage] = useState(3);
+const [data, setData] = useState([]);
+  const [page, setpage] = useState(75);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     axios
       .get(
-        `https://meesho-clone-123.herokuapp.com/proudcts?page=${page}&size=15`
+        `https://fake-rjson-server-pro.herokuapp.com/products`
       )
       .then((data) => {
-        setData(data.data.product);
+        setData(data.data.slice(page, 15+page));
+        // console.log(data.data)
         setLoading(false);
       });
   }, [page]);
